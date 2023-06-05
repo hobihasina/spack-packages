db07387542b0e8542e6822cef8617b75b708ebc6
hobihasina <hobihasina@aims.ac.za>
2023-06-05 13:13:24 +0200

    adding modular package

---
+++ packages/modular/package.py
@@ -0,0 +1,32 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Modular(CMakePackage):
+    """FIXME: Put a proper description of your package here."""
+
+    homepage = "https://github.com/singular-gpispace/modular"
+    git      = "https://github.com/singular-gpispace/modular"
+
+    maintainers = ['hobihasina', 'jankoboehm']
+
+    version('latest', branch='main')
+
+    depends_on('singular@snapshot_22_03')
+    depends_on('gpi-space@22.09:')
+
+    def cmake_args(self):
+        spec = self.spec
+        print(self.spec)
+        args = [ self.define("GSPC_HOME", spec['gpi-space'].prefix)
+               , self.define("SINGULAR_HOME", spec['singular'].prefix)
+               ]
+        return args
+
+    def setup_run_environment(self, env):
+        spec = self.spec
+        env.set('SINGULARPATH', self.prefix)
